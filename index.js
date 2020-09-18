e1887d43c086b737db017a37304381fec38a3ee4
meet <meet@meets-MacBook-Air.local>
2020-09-18 15:40:15 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function redirect() {
     var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
     var today = days[new Date().getDay()];
-    var default_string = 'https://mit-bhalodi.github.io/time-table/'
-    var new_string = default_string + today + '.html'
-    window.location.replace(new_string);
+    if (today == 'sunday') {
+        alert('no lectures on sunday');
+        return;
+    }
+    else{
+        var default_string = 'https://mit-bhalodi.github.io/time-table/'
+        var new_string = default_string + today + '.html'
+        window.location.replace(new_string);
+    }
 }
