d2153daa300c2b99b0d6ebc68bd410ec48add7d0
meet <meet@meets-MacBook-Air.local>
2020-09-25 17:20:22 +0530

    new version

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-setTimeout( function () {
-    var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-    var today = days[new Date().getDay()];
-    if (today == 'sunday') {
-        window.location.replace('https://mit-bhalodi.github.io/time-table/monday.html');
-    }
-    else{
-        var default_string = 'https://mit-bhalodi.github.io/time-table/'
-        var new_string = default_string + today + '.html'
-        window.location.replace(new_string);
-    }
-}, 1000)
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
